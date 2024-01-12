c558db3342b6a51b93e24e5057e477a3708481f9
Chayan Dutta <dipakchatterjee957@gmail.com>
2024-01-12 21:30:32 +0530

    retry rxjs

--- src/app/rxjs/retry/retry.component.ts
+++ src/app/rxjs/retry/retry.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { delay, retry, retryWhen, scan } from 'rxjs';
 
 @Component({
   selector: 'app-retry',
@@ -9,17 +10,43 @@ import { Component, OnInit } from '@angular/core';
 export class RetryComponent implements OnInit {
 
   tableData: any;
+  fetching: boolean = false;
+  status: string = 'No Data';
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
-    this.fetchDetails();
   }
 
   fetchDetails() {
-    this.http.get('https://jsonplaceholder.typicode.com/posts').subscribe(res => {
+    this.fetching = true;
+    this.status = 'Fetching Data ...';
+    this.http.get('https://jsonplaceholder.typicode.com/posts').pipe(
+      // retry(3);
+      retryWhen(err => err.pipe(
+        delay(2000),
+        scan((acc, cur, index) => {
+          if (index >= 5) {
+            console.log(err)
+            throw err;
+          }
+          else {
+            console.log(acc, cur)
+            this.status = 'Retrying Attempt # ' + index;
+          }
+        })
+      ))
+    ).subscribe(res => {
       console.log(res);
       this.tableData = res;
+      this.fetching = false;
+      this.status = 'Data Fetched';
+    }, (err) => {
+      console.log(err);
+      this.tableData = [];
+      this.fetching = false;
+      this.status = 'Problem Fetching Data'
     })
   }
 }
+
