ac284c61a2265f2023ce1d2d6c4a2aa4867a38e1
chayandatta <chayan.datta@bandhan.org>
2024-01-12 14:21:51 +0530

    take rxjs

--- src/app/rxjs/take/take.component.ts
+++ src/app/rxjs/take/take.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { from, fromEvent, interval, map, take, takeLast, takeUntil, toArray } from 'rxjs';
+import { from, fromEvent, interval, map, take, takeLast, takeUntil, takeWhile, tap, toArray } from 'rxjs';
 import { UtilityService } from 'src/app/appService/utility.service';
 
 @Component({
@@ -18,14 +18,14 @@ export class TakeComponent implements OnInit {
     obs1.pipe(
       take(3)
     ).subscribe(res => {
-      console.log(res);
+      // console.log(res);
       this._utility.addListItem(res, 'container1');
     })
 
     // Ex - 2
     const obs2 = from(array);
     obs2.pipe(
-      takeLast(3)
+      takeWhile((x, index) => index < 3),
     ).subscribe(res => {
       console.log(res);
       this._utility.addListItem(res, 'container2');
@@ -33,15 +33,24 @@ export class TakeComponent implements OnInit {
 
     // Ex - 3
     // After every 1 sec value is emiting. When we click anywhere in the webpage 
-    // the takeUntil function is running and the insert value operation was stopped.
+    // the takeUntil function condition false and the insert value operation was stopped.
     const obs3 = interval(1000);
     let conditionFun = fromEvent(document, 'click')
     obs3.pipe(
       map(data => "Item" + data),
       takeUntil(conditionFun)
     ).subscribe(res => {
-      console.log(res);
+      // console.log(res);
       this._utility.addListItem(res, 'container3');
+    })
+
+    // Ex - 4
+    const obs4 = from(array);
+    obs4.pipe(
+      takeWhile((x, index) => index < 4),
+    ).subscribe(res => {
+      console.log(res);
+      this._utility.addListItem(res, 'container4');
     })
   }
 }
