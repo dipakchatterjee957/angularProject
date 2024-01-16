50795609f53f0c6eb7f9665f495a8b14cba2f0e0
Chayan Dutta <dipakchatterjee957@gmail.com>
2024-01-17 00:19:03 +0530

    replay subject

--- src/app/rxjs/replay-subject/replay-subject.component.ts
+++ src/app/rxjs/replay-subject/replay-subject.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Subscription, interval } from 'rxjs';
 import { UtilityService } from 'src/app/appService/utility.service';
 
 @Component({
@@ -8,12 +9,19 @@ import { UtilityService } from 'src/app/appService/utility.service';
 })
 export class ReplaySubjectComponent implements OnInit {
 
+  @ViewChild('myInput') myInput: ElementRef;
   userList_1: Array<any> = [];
   userList_2: Array<any> = [];
-  userList_3: Array<any> = ['csdvc', 'iuyyu'];
+  userList_3: Array<any> = [];
 
   subscribeMode_2: boolean = false;
   subscribeMode_3: boolean = false;
+
+  subscriptionUser_2: Subscription;
+  subscriptionUser_3: Subscription;
+
+  methodChange: boolean = true;
+  intervalSubscription: Subscription;
 
   constructor(public _utility: UtilityService) { }
 
@@ -25,17 +33,41 @@ export class ReplaySubjectComponent implements OnInit {
   }
 
   onAddVideo(data: string) {
-    this._utility.videoEmit.next(data)
+    this._utility.videoEmit.next(data);
+    this.myInput.nativeElement.value = '';
   }
 
   userTwoSubscribe() {
-    this._utility.videoEmit.subscribe(res => {
-      this.userList_2.push(res);
-    })
+    if (this.subscribeMode_2) {
+      this.subscriptionUser_2.unsubscribe();
+    } else {
+      this.subscriptionUser_2 = this._utility.videoEmit.subscribe(res => {
+        this.userList_2.push(res);
+      })
+    }
     this.subscribeMode_2 = !this.subscribeMode_2
   }
 
   userThreeSubscribe() {
+    if (this.subscribeMode_3) {
+      this.subscriptionUser_3.unsubscribe();
+    } else {
+      this.subscriptionUser_3 = this._utility.videoEmit.subscribe(res => {
+        this.userList_3.push(res);
+      })
+    }
     this.subscribeMode_3 = !this.subscribeMode_3
+  }
+
+  toggleMethod() {
+    const broadCastVideo = interval(2000);
+    if (this.methodChange) {
+      this.intervalSubscription = broadCastVideo.subscribe(res => {
+        this._utility.videoEmit.next('Video ' + res)
+      })
+    } else {
+      this.intervalSubscription?.unsubscribe();
+    }
+    this.methodChange = !this.methodChange;
   }
 }
